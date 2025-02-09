2c1a3fc49b926eee86db0be278f03e2f7151413e
Okary <okary@ya.ru>
2025-02-09 14:13:51 +0300

    yml resolve2

--- playwright.config.js
+++ playwright.config.js
@@ -30,7 +30,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
     browserName: 'chromium',
-    headless: false,
+    headless: true,
     baseURL: 'https://authenticationtest.com',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
